edb362720848b5da5139a00d0a5648f02559c1cd
samvar <121091343+samuvara@users.noreply.github.com>
2026-03-16 15:43:23 +0100

    cambiate icone delle recensioni differenziando gatti e cani a modo

--- js/reviews-data.js
+++ js/reviews-data.js
@@ -2,7 +2,7 @@
 const REVIEWS_DATA = [
     {
         author: 'Evelin Guarna',
-        avatar: '🐕',
+        avatar: '🐶',
         rating: 5,
         text: 'Super super carini letizia e Samuel amano gli animali e si sente, il mio cucciolo (maltipoo) li adora entrambi, io da proprietaria posso dire che sono seri, puntuali ed affidabili, che non è poco. Li consiglio vivamente',
         service: 'Dog Sitting',
@@ -10,7 +10,7 @@ const REVIEWS_DATA = [
     },
     {
         author: 'Vanna Cialdoni',
-        avatar: '🐕',
+        avatar: '🐶',
         rating: 5,
         text: 'Mi sono affidata ai servizi di Letizia e Samuel durante una breve vacanza dove non potevo portare con me i miei amici pelosetti, loro venivano tutti i giorni a casa, due volte al giorno, davano loro da mangiare, gli facevano tante coccole e li portavano a passeggio, ero tranquilla perché mi mandavano anche foto e video dove vedevo i miei cagnolini felici, sono molto contenta, ero titubante perché sono cani di grossa taglia, ma hanno gestito tutto ottimamente',
         service: 'Dog Sitting',
@@ -18,7 +18,7 @@ const REVIEWS_DATA = [
     },
     {
         author: 'Irene',
-        avatar: '🐈',
+        avatar: '😺',
         rating: 5,
         text: 'Letizia e Samuel sono stati bravissimi e super affidabili, hanno guardato e provveduto alle mie due gatte durante un breve periodo in cui ho dovuto assentarmi e non ho potuto portarli con me. Mandano tante foto e video se richiesti con aggiornamenti tempestivi sui nostri animali, così mi sento tranquilla a lasciarli per qualche giorno! Sono garantite anche ore di gioco e svago, somministrazione del cibo e treats vari e pulizia lettiera.',
         service: 'Cat Sitting',
@@ -26,7 +26,7 @@ const REVIEWS_DATA = [
     },
     {
         author: 'Viola Tenace',
-        avatar: '🐕',
+        avatar: '🐶',
         rating: 5,
         text: 'Ragazza fantastica! Mi ha aiutato a portare fuori la Zoe tutte le volte che ne avevo bisogno e mi mandava sempre tante foto. Grazie ancora !!',
         service: 'Dog Sitting',
@@ -34,7 +34,7 @@ const REVIEWS_DATA = [
     },
     {
         author: 'Cristina Claudia Cottini',
-        avatar: '🐕',
+        avatar: '🐶',
         rating: 5,
         text: 'Letizia con Tommy e fantastica,poi sempre puntuale e disponibile,non mi ha mai detto di no con i turni che faccio poi.La consiglio.',
         service: 'Dog Sitting',
